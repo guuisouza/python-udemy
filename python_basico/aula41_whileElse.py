9cb55756b42989b79d5f478462804204a361c02e
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2023-11-02 12:15:53 -0300

    WhileElse

---
+++ python_basico/aula41_whileElse.py
@@ -0,0 +1,20 @@
+""" while/else
+    O else não é executado quando tem um break, ele não vai chegar no else.
+"""
+
+string = "Valor qualquer"
+print(len(string))
+
+# Pra que seria um uso desse else:
+
+i = 0
+while i < len(string):
+    letra = string[i]
+    
+    if letra == " ":
+        break # Não será executado o else pois aqui acaba o programa (Se tiver espaço)
+
+    print(letra)
+    i += 1
+else:  # Acontece quando não tem espaço, o If nao valida e vem parar aqui
+    print("Não encontrei espaço")
